05d9b48e3930b61bfaaa85755ce95778f51b0e83
Alex <72355308+alexwith@users.noreply.github.com>
2021-04-15 08:01:48 +0200

    Fix wrong implementation

--- queue-node/src/main/java/me/hyfe/queuenode/priority/permissions/LuckPermsPermissionPredicate.java
+++ queue-node/src/main/java/me/hyfe/queuenode/priority/permissions/LuckPermsPermissionPredicate.java
@@ -4,16 +4,15 @@ import net.luckperms.api.LuckPerms;
 import net.luckperms.api.LuckPermsProvider;
 import net.luckperms.api.model.user.User;
 import net.luckperms.api.model.user.UserManager;
-
-import java.util.UUID;
+import org.bukkit.entity.Player;
 
 public class LuckPermsPermissionPredicate implements PermissionPredicate {
     private final LuckPerms api = LuckPermsProvider.get();
     private final UserManager userManager = this.api.getUserManager();
 
     @Override
-    public boolean test(UUID uuid, String permission) {
-        User user = this.userManager.getUser(uuid);
+    public boolean test(Player player, String permission) {
+        User user = this.userManager.getUser(player.getUniqueId());
         if (user == null) {
             return false;
         }
